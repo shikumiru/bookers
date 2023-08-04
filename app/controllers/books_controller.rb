901e3f66734ab785421886cba7ac33b8cdc58b98
shikumiru <138570859+shikumiru@users.noreply.github.com>
2023-08-04 05:40:08 +0000

    [MoreUpdate] new & create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    book.save
+    @book.save
     redirect_to book_path(@book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
